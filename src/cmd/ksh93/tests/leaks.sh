38de1f58fb8c1ed47028a1554d4f764c01449fd6
Martijn Dekker <martijn@inlv.org>
2020-07-01 15:08:49 +0200

    tests/leaks.sh: portability tweaks

--- src/cmd/ksh93/tests/leaks.sh
+++ src/cmd/ksh93/tests/leaks.sh
@@ -32,8 +32,13 @@ integer Errors=0
 # Get the current amount of memory usage
 function getmem
 {
-	echo $(ps -p $$ -o pid,vsz | awk '{ if (NR != 1){ print $2 } }')
+	UNIX95=1 ps -p "$$" -o vsz=
 }
+n=$(getmem)
+if	! let "$n == $n" 2>/dev/null	# not a number?
+then	err\_exit "$LINENO" "'ps' not POSIX-compliant; skipping tests"
+	exit 0
+fi
 
 # test for variable reset leak #
 
@@ -51,13 +56,13 @@ n=1000
 # one round to get to steady state -- sensitive to -x
 
 test_reset $n
-before="$(getmem)"
-after="$(getmem)"
+before=$(getmem)
+after=$(getmem)
 
 test_reset $n
-before="$(getmem)"
+before=$(getmem)
 test_reset $n
-after="$(getmem)"
+after=$(getmem)
 
 if	(( after > before ))
 then	err_exit "variable value reset memory leak -- $((after - before)) bytes after $n iterations"
@@ -72,21 +77,21 @@ done
 
 data="(v=;sid=;di=;hi=;ti='1328244300';lv='o';id='172.3.161.178';var=(k='conn_num._total';u=;fr=;l='Number of Connections';n='22';t='number';))"
 read -C stat <<< "$data"
-before="$(getmem)"
+before=$(getmem)
 for ((i=0; i < 500; i++))
 do	print -r -- "$data"
 done |	while read -u$n -C stat
 	do	:
 	done	{n}<&0-
-after="$(getmem)"
+after=$(getmem)
 (( after > before )) && err_exit 'memory leak with read -C when deleting compound variable'
 
 read -C stat <<< "$data"
-before="$(getmem)"
+before=$(getmem)
 for ((i=0; i < 500; i++))
 do      read -C stat <<< "$data"
 done
-after="$(getmem)"
+after=$(getmem)
 (( after > before )) && err_exit 'memory leak with read -C when using <<<'
 
 exit $((Errors<125?Errors:125))
