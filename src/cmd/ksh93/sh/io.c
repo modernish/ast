57ccb211307be66be3d4f65c9a15175eab6004dc
Martijn Dekker <martijn@inlv.org>
2021-04-03 07:28:23 +0100

    Backport https://github.com/att/ast/issues/399

--- src/cmd/ksh93/sh/io.c
+++ src/cmd/ksh93/sh/io.c
@@ -2098,7 +2098,7 @@ static int	io_prompt(Shell_t *shp,Sfio_t *iop,register int flag)
 	{
 		case 1:
 		{
-			register int c;
+			int c, escseq = 0;
 #if defined(TIOCLBIC) && defined(LFLUSHO)
 			if(!sh_isoption(SH_VI) && !sh_isoption(SH_EMACS) && !sh_isoption(SH_GMACS))
 			{
@@ -2114,18 +2114,21 @@ static int	io_prompt(Shell_t *shp,Sfio_t *iop,register int flag)
 			shp->exitval = 0;
 			for(;c= *cp;cp++)
 			{
-				if(c==HIST_CHAR)
+				/* don't expand ! in an escape sequence */
+				if((escseq==0 && c==ESC) || (escseq==1 && (c=='[' || c==']' || c=='(')))
+					escseq++;
+				else if(c==HIST_CHAR && escseq==0)
 				{
 					/* look at next character */
 					c = *++cp;
 					/* print out line number if not !! */
 					if(c!= HIST_CHAR)
-					{
 						sfprintf(sfstderr,"%d", shp->gd->hist_ptr?(int)shp->gd->hist_ptr->histind:++cmdno);
-					}
 					if(c==0)
 						goto done;
 				}
+				else
+					escseq = 0;
 				sfputc(sfstderr,c);
 			}
 			goto done;
